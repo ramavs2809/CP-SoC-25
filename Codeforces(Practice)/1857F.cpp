60ba5ef13d9168211e63b7e1d658bebb3f100d46
Your Name <you@example.com>
2025-06-26 23:34:20 +0530

    second

---
+++ Codeforces(Practice)/1857F.cpp
@@ -0,0 +1,93 @@
+//https://codeforces.com/problemset/problem/1857/F
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace std::chrono;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+#define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define MOD 1000000007
+#define DIV 998244353
+#define INF 1e18
+#define vi vector<int>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pll pair<ll, ll>
+#define v(x) vector<x>
+#define fo(i, n) for (decltype(n) i = 0; i < n; i++)
+#define re(i, n) for (decltype(n) i = n - 1; i >= 0; i--)
+#define fo1(i, a, b) for (decltype(b) i = a; i < b; i++)
+#define re1(i, a, b) for (decltype(a) i = a; i >= b; i--)
+#define YN(possible) cout << ((possible) ? "YES" : "NO") << endl;
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define F first
+#define S second
+#define pb push_back
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define read(a, n) for (int i = 0; i < n; ++i) cin >> a[i];
+#define print_space(a, n) for (int i = 0; i < n; ++i) cout << a[i] << (i == n - 1 ? "\n" : " ");
+bool fpc(pii x,pii y){
+    if(x.first!=y.first){return x.first<y.first;}
+    else{return x.second<y.second;}
+}
+bool is_square(ll x){
+    ll y=sqrt(x);
+    return y*y==x;
+}
+void solve(){
+    int n;
+    cin>>n;
+    map<ll,ll> m;
+    ll a;
+    int ar[n];
+    fo(i,n){
+        cin>>a;
+        ar[i]=a;
+        m[a]++;
+    }
+    int q;
+    cin>>q;
+    ll x,y;
+    fo(i,q){
+        cin>>x>>y;
+        if(((x*x)-(4*y))==0){
+            if(x%2==0){cout<<(m[x/2]*(m[x/2]-1))/2<<" ";}
+            else{cout<<"0 ";}
+        }
+        else if(((x*x)-(4*y))>0){
+            if(is_square(((x*x)-(4*y)))){
+                ll j=sqrt(((x*x)-(4*y)));
+                if((j+x)%2==0){
+                    cout<<(m[(j+x)/2]*m[(x-j)/2])<<" ";
+                }
+                else{cout<<"0 ";}
+            }
+            else{cout<<"0 ";}
+        }
+        else{cout<<"0 ";}
+    }
+    cout<<"\n";
+}
+int main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+fastio;
+int t=1;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
