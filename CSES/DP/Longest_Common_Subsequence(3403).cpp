041273c8592583e8ef781e071428857d49e6260f
Your Name <you@example.com>
2025-07-03 01:48:59 +0530

    cses

---
+++ CSES/DP/Longest_Common_Subsequence(3403).cpp
@@ -0,0 +1,74 @@
+//https://cses.fi/problemset/task/3403
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace std::chrono;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+#define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define MOD 1000000007
+#define INF 1e18
+#define DIV 998244353
+#define vi vector<int>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pll pair<ll, ll>
+#define v(x) vector<x>
+#define fo(i, n) for (decltype(n) i = 0; i < n; i++)
+#define re(i, n) for (decltype(n) i = n - 1; i >= 0; i--)
+#define fo1(i, a, b) for (decltype(b) i = a; i < b; i++)
+#define re1(i, a, b) for (decltype(a) i = a; i >= b; i--)
+#define YN(possible) cout << ((possible) ? "YES" : "NO") << endl;
+#define pb push_back
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define read(a, n) for (int i = 0; i < n; ++i) cin >> a[i];
+#define print_space(a, n) for (int i = 0; i < n; ++i) cout << a[i] << (i == n - 1 ? "\n" : " ");
+void solve(){ 
+    int n,m;
+    cin>>n>>m;
+    int a[n],b[m];
+    read(a,n);read(b,m);
+    vvi c(n + 1, vi(m + 1, 0));
+    fo(i,n+1){c[i][0]=0;}
+    fo(i,m+1){c[0][i]=0;}
+    fo1(i,1,n+1){
+        fo1(j,1,m+1){
+            if(a[i-1]==b[j-1]){
+                c[i][j]=1+c[i-1][j-1];
+            }
+            else{c[i][j]=max(c[i][j-1],c[i-1][j]);}
+        }
+    }
+    cout<<c[n][m]<<"\n";
+    int i=n,j=m;
+    vi v;
+    while((i>0)&&(j>0)){
+        if(a[i-1]==b[j-1]){v.pb(a[i-1]);i--;j--;}
+        else if(c[i-1][j]>c[i][j-1]){i--;}
+        else{j--;}
+    }
+    while(!v.empty()){cout<<v.back()<<" ";v.pop_back();}
+    cout<<"\n";
+}
+int main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+fastio;
+int t=1;
+// cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
