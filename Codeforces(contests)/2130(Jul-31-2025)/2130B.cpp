870afd6103a301b03e624cb845e1e7df09bc2907
Your Name <you@example.com>
2025-07-31 23:11:36 +0530

    .

---
+++ Codeforces(contests)/2130(Jul-31-2025)/2130B.cpp
@@ -0,0 +1,66 @@
+//https://codeforces.com/contest/2130/problem/B
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace std::chrono;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+#define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define MOD 1000000007
+#define INF 1e18
+#define DIV 998244353
+#define vi vector<int>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pll pair<ll, ll>
+#define v(x) vector<x>
+#define fo(i, n) for (decltype(n) i = 0; i < n; i++)
+#define re(i, n) for (decltype(n) i = n - 1; i >= 0; i--)
+#define fo1(i, a, b) for (decltype(b) i = a; i < b; i++)
+#define re1(i, a, b) for (decltype(a) i = a; i >= b; i--)
+#define YN(possible) cout << ((possible) ? "YES" : "NO") << endl;
+#define pb push_back
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define read(a, n) for (int i = 0; i < n; ++i) cin >> a[i];
+#define print_space(a, n) for (int i = 0; i < n; ++i) cout << a[i] << (i == n - 1 ? "\n" : " ");
+void solve(){
+    int n,s;
+    cin>>n>>s;
+    int a[n];
+    read(a,n);
+    int sum=0;
+    map<int,int> m;
+    fo(i,n){sum+=a[i];m[a[i]]++;}
+    if(sum>s){print_space(a,n);}
+    else{
+        if(s==(sum+1)){
+            while(m[0]--){cout<<"0 ";}
+            while(m[2]--){cout<<"2 ";}
+            while(m[1]--){cout<<"1 ";}
+            cout<<"\n";
+        }
+        else{cout<<"-1\n";}
+    }
+}
+int main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+fastio;
+int t=1;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
